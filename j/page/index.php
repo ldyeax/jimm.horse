efdcd9571d242c1f378209ac52183b945ebdbc46
jimm <ldyeax@gmail.com>
2023-03-04 13:35:36 -0500

    /

--- j/page/index.php
+++ j/page/index.php
@@ -4,5 +4,5 @@
 }
 </style>
 <div id=homepage>
-	<img class="cookie_webp" src="img/cookie_transparent.webp" alt="My OC floating on a cookie by Tiny Jasmini" longdesc="https://derpibooru.org/images/3055814">
+	<img class="cookie_webp" src="/img/cookie_transparent.webp" alt="My OC floating on a cookie by Tiny Jasmini" longdesc="https://derpibooru.org/images/3055814">
 </div>
