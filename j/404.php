d15646eb97f9bbc10ce15c8c9dbab3a8ea582fad
jimm <ldyeax@gmail.com>
2023-03-05 03:42:22 -0500

    annie 404

--- j/404.php
+++ j/404.php
@@ -5,4 +5,12 @@ header("HTTP/1.0 404 Not Found");
 <meta charset=utf-8>
 <title>404</title>
 <body>
-File Not Found
+<canvas id=annie></canvas>
+<script type=module>
+import Matrix from "https://raw.githubusercontent.com/ldyeax/Fill-In-Matrix-Effect/main/matrix.js";
+Matrix({
+    canvas: document.getElementById("annie"),
+    imageUrl: "/img/annie_green.png",
+    fullscreen: true
+});
+</script>
