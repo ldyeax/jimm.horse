0af2835d603870d2b609a9cb08526a3400f846e2
jimm <ldyeax@gmail.com>
2023-03-13 11:16:25 -0400

    secret

---
+++ secret/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<meta charset=utf-8>
+<title>shh</title>
+<style>
+body {
+	background-image: url("/img/day_133__spyro___midnight_mountain__35_minutes__by_cryptid_creations_d604yxg.png");
+	background-size: cover;
+}
+</style>
+<body>
+<!-- https://www.deviantart.com/cryptid-creations/art/DAY-133-Spyro-Midnight-Mountain-35-Minutes-363028948 -->
