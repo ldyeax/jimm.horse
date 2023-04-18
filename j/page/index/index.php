c437b4891348c0ca3dfc6b58413ff12dc99cee60
Jimm <jimmythetronlegacy@gmail.com>
2023-04-18 00:31:18 -0400

    change background image

--- j/page/index/index.php
+++ j/page/index/index.php
@@ -1,6 +1,9 @@
 
 <div id=homepage>
-	<div id=overlay1><img class="cookie_webp" src="/img/cookie_transparent.webp" alt="My OC floating on a cookie by Tiny Jasmini" data-source="https://derpibooru.org/images/3055814"></div>
+	<div id=overlay1>
+		<!--<img class="cookie_webp" src="/img/cookie_transparent.webp" alt="My OC floating on a cookie by Tiny Jasmini" data-source="https://derpibooru.org/images/3055814">-->
+		<img src="/img/jimm.webp" alt="My OC">
+	</div>
 	<div id=overlay2 aria-hidden="true">
 	</div>
 
