e401a496ceb73cfa8ef2e25321b0d38dcbb1093e
Jimm <jimmythetronlegacy@gmail.com>
2023-04-20 23:43:56 -0400

    github, htaccess, email

--- j/component/header.php
+++ j/component/header.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
 <meta charset=utf-8>
+<meta name="author" content="jimm@jimm.horse">
 <link rel="icon" type="image/png" href="/img/excited.png">
 <!--
 <?php echo file_get_contents("$jroot/component/pudim.txt"); ?>
